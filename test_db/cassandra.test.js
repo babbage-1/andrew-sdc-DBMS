1ea837f36ec666fb08cc6b961f4ffb8d092ca0d4
ahchung23 <hahneol@gmail.com>
2019-04-23 21:56:16 -0400

    add tests for cassandra
    test if query returns correct object and is under 50ms

--- test_db/cassandra.test.js
+++ test_db/cassandra.test.js
@@ -1,38 +1,87 @@
+/* eslint-disable no-await-in-loop */
 /* eslint-disable block-scoped-var */
 /* eslint-disable vars-on-top */
 /* eslint-disable no-var */
 const cassandra = require('cassandra-driver');
 
-const client = new cassandra.Client({
+var client = new cassandra.Client({
   contactPoints: ['localhost'],
   localDataCenter: 'datacenter1',
+  keyspace: 'sdc_andrew',
 });
 
-async function connectAndCheckCassandra() {
-  try {
-    var t = process.hrtime();
-    await client.connect();
-    console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
-    client.hosts.forEach((host) => {
-      console.log(host.address, host.datacenter, host.rack, host.isUp(), host.canBeConsideredAsUp());
-    });
-    await client.shutdown();
-    console.log('client shutdown');
-  } catch (e) {
-    console.log(e);
-    throw e;
-  } finally {
-    await client.shutdown();
-    console.log('client shutdown');
-    var tEnd = process.hrtime(t);
-    console.log('time end', tEnd);
-  }
-}
-connectAndCheckCassandra();
+beforeAll(async () => {
+  await client.connect();
+  console.log('client connected');
+});
 
-function sum(a, b) {
-  return a + b;
-}
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+afterAll(async () => {
+  await client.shutdown();
+  console.log('client shutdown');
+});
+
+describe('connection to Cassandra established', () => {
+  test('can output connection details', async () => {
+    try {
+      console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
+      client.hosts.forEach((host) => {
+        console.log(host.address, host.datacenter, host.rack, host.isUp(), host.canBeConsideredAsUp());
+      });
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  });
+});
+
+describe('Cassandra DBMS Benchmarking with 10M data points', () => {
+  test('Reading movie info from DB returns correct data structure', async () => {
+    try {
+      const query = 'SELECT * FROM movieinfo WHERE id = 1 ALLOW FILTERING';
+      const result = await client.execute(query);
+      const dataObj = result.rows[0];
+      console.log('typeof dataObj', typeof dataObj);
+
+      expect(dataObj).toEqual(expect.objectContaining({
+        genre: expect.any(String),
+        id: expect.any(Number),
+        image: expect.any(String),
+        name: expect.any(String),
+        rating: expect.any(String),
+        releaseday: expect.any(Number),
+        releasemonth: expect.any(String),
+        releaseyear: expect.any(Number),
+        runtime: expect.any(Number),
+        score: expect.any(Number),
+      }));
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  });
+
+  test('Reading movie info from database takes less than 50ms on average for 1000 queries', async () => {
+    // intialize time array
+    let totalTimeInMs = 0;
+    // iterate through last 100 data points in db
+    for (var i = (9999000); i <= (10000000); i += 1) {
+      const query = 'SELECT * FROM movieinfo WHERE id = ? ALLOW FILTERING';
+      const params = [i];
+
+      // measure time to finish retreiving result for query
+      const t = process.hrtime();
+      const result = await client.execute(query, params, { hints: ['int'], prepare: true });
+      if (i === 9999000) {
+        console.log('confirm async times being measured!\n', result.rows[0]);
+      }
+      const tEnd = process.hrtime(t);
+      // push time instance to time array after converting nanoseconds to milliseconds
+      totalTimeInMs += (tEnd[1] / 1000000);
+    }
+
+    // expect average to be less than 50ms
+    const average = totalTimeInMs / 1000;
+    console.log('WHAT IS AVERAGE QUERY TIME?\n', average);
+    expect(average).toBeLessThan(50);
+  });
 });
