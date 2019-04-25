9e3b5c0f0a265773f63702406e48f7ad04c73e3d
ahchung23 <hahneol@gmail.com>
2019-04-24 23:27:21 -0400

    change seedPostgreSQL.js to use config from config.js
    config.js uses variables from postgres.env

---
+++ seed_database/config.js
@@ -0,0 +1,16 @@
+/* eslint-disable global-require */
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: './postgres.env' });
+}
+
+const config = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+};
+
+module.exports = {
+  config,
+};
